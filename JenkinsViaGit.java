ab2984537b3cf6f60dd2f1f8a0e24647ac7f7d2f
Janakrishnan Murugesan <68744155+JK-Code22@users.noreply.github.com>
2023-04-04 11:39:09 +0530

    updated

--- JenkinsViaGit.java
+++ JenkinsViaGit.java
@@ -5,6 +5,6 @@ public class JenkinsViaGit{
     System.out.println("Test1");
     System.out.println("Test2");
 		System.out.println("Test3 - new commit and push to trigger jenkins build");
-		System.out.println("Test4 - new commit and push to trigger jenkins build1");
+		System.out.println("Test4 - new commit and push to trigger jenkins build2");
 	}
 }
