d4a87863c701452bb0b4295f330d906b01546e66
Janakrishnan Murugesan <68744155+JK-Code22@users.noreply.github.com>
2023-04-04 11:15:57 +0530

    Initial commit

---
+++ JenkinsViaGit.java
@@ -0,0 +1,8 @@
+public class JenkinsViaGit{
+
+	public static void main(String[] args) {
+		System.out.println("Triggering Jenkins via Git");
+    System.out.println("Test1");
+    System.out.println("Test2");
+	}
+}
